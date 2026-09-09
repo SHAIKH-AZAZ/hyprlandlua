1cb88d312da112480de6ea3581bf0308defb2f2a
SHAIKH-AZAZ <azazshaikh2703@gmail.com>
2026-09-09 22:37:25 +0530

    test commit

--- hyrpland_lua/config.lua
+++ hyrpland_lua/config.lua
@@ -1,23 +1,24 @@
 local home = os.getenv("HOME")
 
 local M = {
-  home = home,
-  paths = {
-    hypr = home .. "/.config/hypr",
-    configs = home .. "/.config/hypr/configs",
-    user_configs = home .. "/.config/hypr/UserConfigs",
-    scripts = home .. "/.config/hypr/scripts",
-    user_scripts = home .. "/.config/hypr/UserScripts",
-    generated = home .. "/.config/hypr/hyrpland_lua/generated",
-  },
+	home = home,
+	paths = {
+		hypr = home .. "/.config/hypr",
+		configs = home .. "/.config/hypr/configs",
+		user_configs = home .. "/.config/hypr/UserConfigs",
+		scripts = home .. "/.config/hypr/scripts",
+		user_scripts = home .. "/.config/hypr/UserScripts",
+		generated = home .. "/.config/hypr/hyrpland_lua/generated",
+	},
 }
 
 M.defaults = {
-  editor = "nvim",
-  terminal = "kitty",
-  file_manager = "thunar",
-  search_engine = "https://www.google.com/search?q={}",
-  main_mod = "SUPER",
+	editor = "nvim",
+	terminal = "kitty",
+	file_manager = "thunar",
+	search_engine = "https://www.google.com/search?q={}",
+	main_mod = "SUPER",
 }
 
 return M
+-- this is test commoent
